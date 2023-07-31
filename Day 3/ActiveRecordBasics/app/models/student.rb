ca8cc07ff547e956bfe70782169051021b908055
noumanshah436 <noumanshah436@gmail.com>
2023-07-31 17:39:31 +0500

    initial commit

---
+++ Day 3/ActiveRecordBasics/app/models/student.rb
@@ -0,0 +1,67 @@
+class GoodnessValidator < ActiveModel::Validator
+  def validate(record)
+    if record.name == "Nouman"
+      record.errors[:base] << "This person is Nouman"
+    end
+  end
+end
+
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.name.starts_with? 'X'
+      record.errors[:base] << 'Need a name starting with X please!'
+    end
+  end
+end
+
+
+
+
+class Student < ApplicationRecord
+  # validates :name, presence: true, allow_blank: true
+  # validates :name, presence: true
+  # validates :name, absence: true
+  # validates :name, uniqueness: true
+  # validates :name, numericality: true
+  # validates_with GoodnessValidator
+  # validates_with MyValidator
+  # validates :name, presence: true, length: { minimum: 3 }
+  # validates :name, presence: { message: "name must be given please" }
+
+  # validate :attachment_content_type    # copied from mrsool
+
+  validate :custom_method
+
+  def custom_method
+    if self.age < 18
+      errors.add(:age, "age should be greater than 18")
+    end
+  end
+
+
+  # def attachment_content_type
+  #   ICON_CONTENT_TYPE_REGEXP = %r{\Aimage/.*\Z}
+  #   return unless icon_ar.attached?
+  #   unless ICON_CONTENT_TYPE_REGEXP.match(icon_ar.content_type)
+  #     errors.add(
+  #       :icon_ar,
+  #       I18n.t("validation_messages.incorrect_attachment_content_type")
+  #     )
+  #   end
+  # end
+
+end
+
+
+
+
+# Student.create!(name: "Nouman")           # this give error on GoodnessValidator
+
+# *********************
+# validate:
+
+#  validate :custom_method -> By default, such validations will run every time you call valid? or save the object.
+# std = Student.create!(name: "nouman", age: 17)    -- give error as customized in custom_method
+
+#  But it is also possible to control when to run these custom validations by giving an :on option to the validate method, with either: :create or :update.
+# validate :custom_method, on: :create
